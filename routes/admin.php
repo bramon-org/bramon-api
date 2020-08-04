8bf08450679ead2f0985f6e857f182d2294c13d3
Thiago Paes <mrprompt@gmail.com>
2020-08-03 21:03:22 -0300

    Admin can show stations

--- routes/admin.php
+++ routes/admin.php
@@ -26,4 +26,5 @@ Route::group(['prefix' => 'v1/admin', 'namespace' => 'Admin', 'middleware' => ['
     Route::post('stations', 'StationController@create');
     Route::get('stations/{id}', 'StationController@show');
     Route::put('stations/{id}', 'StationController@update');
+    Route::get('stations/{id}/list', 'StationController@fromUser');
 });
