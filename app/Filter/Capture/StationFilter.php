802841b209f4d648c81dd0f63667e4b9f859735f
Thiago Paes <mrprompt@gmail.com>
2020-08-09 02:00:02 -0300

    Add new filter to captures: station

---
+++ app/Filter/Capture/StationFilter.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filter\Capture;
+
+use Fouladgar\EloquentBuilder\Support\Foundation\Contracts\Filter;
+use Illuminate\Database\Eloquent\Builder;
+
+class StationFilter extends Filter
+{
+    /**
+     * Apply the captured_at condition to the query.
+     *
+     * @param Builder $builder
+     * @param mixed   $value
+     *
+     * @return Builder
+     */
+    public function apply(Builder $builder, $value): Builder
+    {
+        return $builder->whereIn('station_id', $value);
+    }
+}
