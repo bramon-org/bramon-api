917a0d8a8ef1bdcadcadaf36e4a2f18a64b775b5
Thiago Paes <mrprompt@gmail.com>
2020-09-16 16:59:47 -0300

    More filters

---
+++ app/Filter/Station/CountryFilter.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filter\Station;
+
+use Fouladgar\EloquentBuilder\Support\Foundation\Contracts\Filter;
+use Illuminate\Database\Eloquent\Builder;
+
+class CountryFilter extends Filter
+{
+    /**
+     * Apply the source condition to the query.
+     *
+     * @param Builder $builder
+     * @param mixed   $value
+     *
+     * @return Builder
+     */
+    public function apply(Builder $builder, $value): Builder
+    {
+        return $builder->where('country', '=', $value);
+    }
+}
