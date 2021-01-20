a6b34e625d27006b9bc1a25c7cd86e1b2ac2e812
Thiago Paes <mrprompt@gmail.com>
2021-01-20 02:18:51 -0300

    fix migration

--- database/migrations/2021_01_19_190653_change-users-table.php
+++ database/migrations/2021_01_19_190653_change-users-table.php
@@ -13,7 +13,7 @@ class ChangeUsersTable extends Migration
      */
     public function up()
     {
-        Schema::table('files', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->string('mobile_phone', 255)->change();
         });
     }
@@ -25,7 +25,7 @@ class ChangeUsersTable extends Migration
      */
     public function down()
     {
-        Schema::table('files', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->string('mobile_phone', 255)->change();
         });
     }
