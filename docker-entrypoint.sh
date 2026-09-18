b31cf1a23333a1f84b7cd528f52a75c906b847e8
Thiago Paes <mrprompt@gmail.com>
2026-09-17 23:23:36 -0300

    feat: upgrade to PHP 8.4

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,10 @@
 
 app_env=$APP_ENV
 
+find /var/www/html -type d -exec chmod 755 {} +
+find /var/www/html -type f -exec chmod 644 {} +
+chmod -R 775 /var/www/html/storage
+
 if [ $# -eq 0 ]; then
   php artisan migrate --force
   apache2-foreground
